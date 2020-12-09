3f6b3b8efb9676e8966f66383b8284d98b0aff3b
ZCai <zc128@users.noreply.github.com>
2020-12-09 10:48:10 -0500

    Add project files.

---
+++ BasicCalculatorApp/Controllers/CalculatorController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BasicCalculatorApp.Models;
+
+namespace BasicCalculatorApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+        [HttpPost]
+        public IActionResult Index(Calculator cal, string op)
+        {
+
+            switch (op) {
+                case "Add":
+                    cal.result = (cal.firstNum + cal.secondNum).ToString();
+                    break;
+                case "Subtract":
+                    cal.result = (cal.firstNum - cal.secondNum).ToString();
+                    break;
+                case "Multiply":
+                    cal.result = (cal.firstNum * cal.secondNum).ToString();
+                    break;
+                case "Divide":
+                    if (cal.secondNum != 0)
+                    {
+                        cal.result = (cal.firstNum / cal.secondNum).ToString();
+                    }
+                    else {
+                        cal.result = "ERROR: SecondNum CAN'T be 0 in Division!";
+                    }
+                    break;
+                default:
+                    cal.result = 0.ToString();
+                    break;
+            }
+             
+            return View(cal);
+        }
+
+    }
+}
---
+++ BasicCalculatorApp/Models/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicCalculatorApp.Models
+{
+    public class Calculator
+    {
+        public double firstNum { get; set; }
+        public double secondNum { get; set; }
+        public string result { get; set; }
+
+    }
+}
